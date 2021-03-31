49e4dcb3e9796e457c207239d410d6bd70e2631f
hohwang <hohwang@mintech.kr>
2021-03-31 11:03:48 +0900

    until form UI

--- main.js
+++ main.js
@@ -15,9 +15,10 @@ function templateHTML(fileList, title, body){
       <meta charset="utf-8">
     </head>
     <body>
-      <h1><a href="/">WEB3</a></h1>
+      <h1><a href="/">WEB1</a></h1>
       <ul>
         ${list}
+        <li><a href="/create">create</a>
       </ul>
       ${body}
     </body>
@@ -56,7 +57,33 @@ var app = http.createServer(function(request,response){
         })
       }
     }
+    else if ( urlInfo.pathname === '3000/create') {
+      console.log(urlInfo.pathname)
+      fs.readdir('./data', (error, fileList) => {
+        let list = '';
+        for (var i =0; i < fileList.length; i++){
+          list += `<li><a href="/?id=` + `${fileList[i]}">` + `${fileList[i]}` + `</a></li>`;
+        }
+        fs.readFile(`data/${idValue}`, 'utf-8', (err, data) => {
+          content = `
+            <form action="http://localhost:3000/process_create" method="post">
+              <p><input tpye="text" name="title" placeholder="title"></p>
+              <p>
+                <textarea name="description" placeholder="description"></textarea>
+              </p>
+              <p>
+                <input type="submit">
+              </p>
+            </form>
+          `
+          const template = templateHTML(fileList, `WEB1 - ${idValue}`, content)
+          response.writeHead(200);
+          response.end(template);
+        })
+      })
+    }
     else{
+      console.log(`??????`, urlInfo.pathname)
       response.writeHead(404);
       response.end('Not Found');
     }
